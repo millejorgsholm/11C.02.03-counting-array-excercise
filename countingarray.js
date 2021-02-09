2753bb204c747d7453b136efe7e118d4b73131aa
Mille Jørgsholm Bierrings <mill1197@stud.kea.dk>
2021-02-09 20:55:06 +0100

    counting array is working

--- countingarray.js
+++ countingarray.js
@@ -1,1 +1,26 @@
 "use strict";
+
+window.addEventListener("DOMContentLoaded", theArrayCounter);
+
+//The array is const variable because it's not gonna change it's values, however the counter is going to change it's value (count) so it's a let variable
+const arr = [];
+let counter = 0;
+
+function theArrayCounter() {
+  //Adding an item in the beginning of the array with the unShift method
+  arr.unshift(counter);
+  //Adding +1s
+  counter++;
+
+  //Make sure that the array can't contain more than 9 items
+  if (counter > 9) {
+    //If array contains more than 9 items it'll remove the last item, which makes the array shoter. The pop method is used
+    arr.pop(arr);
+  }
+
+  //Setting the counter to 1s (1000ms)
+  setTimeout(theArrayCounter, 1000);
+
+  //Making sure the array is appearing in the browser's console
+  console.log(arr);
+}
