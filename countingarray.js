535c92ed7e43806d8f707995e353bb3a16c84502
Mille Jørgsholm Bierrings <mill1197@stud.kea.dk>
2021-02-11 09:31:24 +0100

    small changes

--- countingarray.js
+++ countingarray.js
@@ -2,19 +2,19 @@
 
 window.addEventListener("DOMContentLoaded", theArrayCounter);
 
-//The array is const variable because it's not gonna change it's values, however the counter is going to change it's value (count) so it's a let variable
+//The array is a const variable because it's not gonna change its values, however the counter is going to change it's value, as it counts, so it's a let variable
 const arr = [];
 let counter = 0;
 
 function theArrayCounter() {
   //Adding an item in the beginning of the array with the unShift method
   arr.unshift(counter);
-  //Adding +1s
+  //Adding +1s - it counts one number
   counter++;
 
-  //Make sure that the array can't contain more than 9 items
+  //Making sure that the array can't contain more than 9 items
   if (counter > 9) {
-    //If array contains more than 9 items it'll remove the last item, which makes the array shorter. The pop method is used
+    //If the array contains more than 9 items it'll remove the last item, which makes the array shorter. The pop method is used
     arr.pop(arr);
   }
 
